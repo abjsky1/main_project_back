da4eab04bb2448006a3a5295163866c4c4cc8317
jaeui777 <jaeui777@gmail.com>
2026-09-23 17:39:16 +0900

    feat : Entity 추가

---
+++ src/main/java/main_project/model/entity/ActionEntity.java
@@ -0,0 +1,29 @@
+package main_project.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity 
+@Table (name = "member_role")
+@AllArgsConstructor 
+@NoArgsConstructor
+@Data 
+@Builder
+public class ActionEntity {
+
+    @Id 
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Integer action_id;
+
+    @Column (length = 50 , nullable = false)
+    private String action_type;
+    
+}
---
+++ src/main/java/main_project/model/entity/Member_roleEntity.java
@@ -0,0 +1,32 @@
+package main_project.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity 
+@Table (name = "member_role")
+@AllArgsConstructor 
+@NoArgsConstructor
+@Data 
+@Builder
+public class Member_roleEntity {
+
+    @Id 
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Integer roel_id;
+
+    @Column (length = 20 , nullable = false, unique = true)
+    private String role_name;
+    
+    @Column  ( length = 100 , nullable = false)
+    private String role_description;
+    
+}
